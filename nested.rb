d152a18518f224e8597e126a231e93fd3af51a30
Claudia Fox <claudiafox95@icloud.com>
2018-11-13 17:56:18 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -16,8 +16,9 @@ def hopper
         }
      }
 
-
+     programmer_hash[:grace_hopper]
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +58,11 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:dennis_ritchie][:languages][0]
+
 end
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -80,7 +87,7 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
